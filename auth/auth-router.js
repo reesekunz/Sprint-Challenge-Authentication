4c52182e97455d5ab62a890386da1be95bfb365b
Reese Kunz <reesekunz21@gmail.com>
2019-09-20 08:30:47 -0700

    register working with hashed password

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,24 +1,23 @@
 const router = require("express").Router();
 
-// const Users = require("../helpers/usersModel");
+const Users = require("../helpers/usersModel");
 const bcrypt = require("bcryptjs");
 const db = require("../database/dbConfig");
 
-const router = express.Router();
 // POST to 3300 api/auth/register
-// router.post("/register", (request, response) => {
-//   let user = request.body;
-//   const hash = bcrypt.hashSync(user.password, 14);
-//   user.password = hash;
+router.post("/register", (request, response) => {
+  let user = request.body;
+  const hash = bcrypt.hashSync(user.password, 14);
+  user.password = hash;
 
-//   Users.add(user)
-//     .then(savedUser => {
-//       response.status(201).json(savedUser);
-//     })
-//     .catch(error => {
-//       response.status(500).json(error);
-//     });
-// });
+  Users.add(user)
+    .then(addedUser => {
+      response.status(201).json(addedUser);
+    })
+    .catch(error => {
+      response.status(500).json(error);
+    });
+});
 
 router.post("/login", (req, res) => {
   // implement login
