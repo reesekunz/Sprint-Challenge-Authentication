3c95fe13d3a5c7fcdd6b0c662c3d5bcd6e74053a
Reese Kunz <reesekunz21@gmail.com>
2019-09-20 08:26:00 -0700

    installed missing dependencies

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,26 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.post('/register', (req, res) => {
-  // implement registration
-});
+// const Users = require("../helpers/usersModel");
+const bcrypt = require("bcryptjs");
+const db = require("../database/dbConfig");
 
-router.post('/login', (req, res) => {
+const router = express.Router();
+// POST to 3300 api/auth/register
+// router.post("/register", (request, response) => {
+//   let user = request.body;
+//   const hash = bcrypt.hashSync(user.password, 14);
+//   user.password = hash;
+
+//   Users.add(user)
+//     .then(savedUser => {
+//       response.status(201).json(savedUser);
+//     })
+//     .catch(error => {
+//       response.status(500).json(error);
+//     });
+// });
+
+router.post("/login", (req, res) => {
   // implement login
 });
 
