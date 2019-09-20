0cfbf5ebd51a8459895df6ebe6f951a9bdcac7c3
Reese Kunz <reesekunz21@gmail.com>
2019-09-20 08:40:42 -0700

    login returning token with valid credentials

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,8 +1,10 @@
-const router = require("express").Router();
-
+const express = require("express");
 const Users = require("../helpers/usersModel");
 const bcrypt = require("bcryptjs");
 const db = require("../database/dbConfig");
+const generateToken = require("../auth/generate-token");
+
+const router = express.Router();
 
 // POST to 3300 api/auth/register
 router.post("/register", (request, response) => {
@@ -19,8 +21,26 @@ router.post("/register", (request, response) => {
     });
 });
 
-router.post("/login", (req, res) => {
-  // implement login
+// POST to 3300 api/auth/login
+router.post("/login", (request, response) => {
+  let { username, password } = request.body;
+  //   console.log("POST login request body", request.body);
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        // console.log("token", token);
+        response.status(200).json({ token });
+      } else {
+        // dont send 404 status because dont want them guessing credentials
+        response.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      response.status(500).json(error);
+    });
 });
 
 module.exports = router;
