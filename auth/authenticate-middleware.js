251e175c947bf4f7a91a9ba4a9fc810467fe31af
Reese Kunz <reesekunz21@gmail.com>
2019-09-20 08:36:18 -0700

    added secrets, token generation, and middleware auth

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,20 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
-
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+const jwt = require("jsonwebtoken");
+const secrets = require("../config/secrets");
+// token verification
+module.exports = (request, response, next) => {
+  const token = request.headers.authorization;
+  if (token) {
+    jwt.verify(token, secrets.jwtSecret, (error, decodedToken) => {
+      if (error) {
+        // token expired or is invalid
+        response.status(401).json({ message: "You shall not pass!" });
+      } else {
+        // token is valid
+        request.user = { username: decodedToken.username };
+        next();
+      }
+    });
+  } else {
+    response.status(400).json({ message: "no credentials provided" });
+  }
 };
