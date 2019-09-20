c60c23d5eb517e6a20c26d0338bd5d9bd8f3e008
Reese Kunz <reesekunz21@gmail.com>
2019-09-20 08:27:45 -0700

    built out users model

---
+++ helpers/usersModel.js
@@ -0,0 +1,38 @@
+const db = require("../database/dbConfig");
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById,
+  remove
+};
+
+function find() {
+  return db("users").select("id", "username");
+}
+
+function findBy(filter) {
+  return db("users").where(filter);
+}
+
+function add(user) {
+  return db("users")
+    .insert(user, "id")
+    .then(ids => {
+      const [id] = ids;
+      return findById(id);
+    });
+}
+
+function findById(id) {
+  return db("users")
+    .where({ id })
+    .first();
+}
+
+function remove(id) {
+  return db("users")
+    .where({ id: id })
+    .del();
+}
